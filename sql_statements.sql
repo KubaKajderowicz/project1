0497e126dd0979a415cd7285a2774c6bd73331c5
KubaKajderowicz <2115394@talnet.nl>
2020-09-09 10:31:29 +0200

    sql statements

---
+++ sql_statements.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY ('id')
+);
+CREATE TABLE `persoon` (
+  `id` int(11) NOT NULL,
+  `voornaam` varchar(255) NOT NULL,
+  `tussenvoegsel` varchar(255) NOT NULL,
+  `achternaam` int(11) NOT NULL,
+  `gebruikersnaam` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  FOREIGN KEY ('account_id') REFERENCES account('id')
+);
